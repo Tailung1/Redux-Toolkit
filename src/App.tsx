56bd8804921e13e87273c42b4ca0189a23b1b967
Tailung1 <ramazsamitashvili11@gmail.com>
2025-04-05 21:49:25 +0400

    move display components logic from main to APP.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import "./App.css";
 
+import "./index.css";
+import { Provider, useSelector } from "react-redux";
+import AccountOperations from "./features/accounts/AccountOperations.tsx";
+import CreateCustomer from "./features/customers/CreateCustomer.tsx";
+import Customer from "./features/customers/Customer.tsx";
+import ShowBalance from "./features/accounts/ShowBalance.tsx";
+import store, { rootReducer } from "./store.ts";
+
+
+
+
 function App() {
-  return <></>;
+    const customer = useSelector(
+      (store: rootReducer) => store.customer
+    );
+  return (
+    <>
+      <Provider store={store}>
+        <CreateCustomer />
+        {customer.fullName && (
+          <>
+            {" "}
+            <Customer />
+            <ShowBalance />
+            <AccountOperations />
+          </>
+        )}
+      </Provider>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { Provider, useSelector } from "react-redux";
-import AccountOperations from "./features/accounts/AccountOperations.tsx";
-import CreateCustomer from "./features/customers/CreateCustomer.tsx";
-import Customer from "./features/customers/Customer.tsx";
-import ShowBalance from "./features/accounts/ShowBalance.tsx";
-import store, { rootReducer } from "./store.ts";
+import App from "./App";
 
-const customer = useSelector((store: rootReducer) => store.customer);
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <CreateCustomer />
-      {customer.fullName && (
-        <>
-          {" "}
-          <Customer />
-          <ShowBalance />
-          <AccountOperations />
-        </>
-      )}
-    </Provider>
+    <App />
   </StrictMode>
 );

f2bd7d7b6d2d8aab3a56c867f0edc8b8cc4f7d9f
Tailung1 <ramazsamitashvili11@gmail.com>
2025-04-07 02:22:34 +0400

    Final Version

--- src/features/accounts/accountSlice.ts
+++ src/features/accounts/accountSlice.ts
@@ -39,19 +39,19 @@ const accountSlice = createSlice({
       },
       reducer(state, action: PayloadAction<payLoadAction>) {
         if (state.loan > 1) return;
-        state.loan = action.payload.amount
-          state.loanPurpose = action.payload.purpose
-          console.log(state.loan)
-          console.log("request");
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        console.log(state.loan);
+        console.log("request");
       },
     },
 
     payLoan(state) {
-        console.log(state.loan);
-        console.log("request");
+      console.log(state.loan);
+      console.log("request");
       if (state.loan > state.balance) return;
       state.balance -= state.loan;
-      state.loan=0;
+      state.loan = 0;
       state.loanPurpose = "";
     },
     convertingCurrency(state) {

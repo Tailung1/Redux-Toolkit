43266f0215e16474f7f5155c4435a4fbf0f722c8
Tailung1 <ramazsamitashvili11@gmail.com>
2025-04-09 19:04:05 +0400

    remove unnecessary spaces

--- src/features/customers/Customer.tsx
+++ src/features/customers/Customer.tsx
@@ -5,7 +5,7 @@ export default function Customer() {
   const costumerName = useSelector(
     (store: rootReducerType) => store.customer.fullName
   );
-  
+
   return (
     <div>
       <h1>Welcome,{costumerName as string}</h1>
